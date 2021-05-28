ad687ceae7fd2aad7c27c6673727d2adaf044094
ybdhani <ramadhani.nurfikri@gmail.com>
2021-05-29 01:26:39 +0800

    change

--- scripts/AboutUsScript.js
+++ scripts/AboutUsScript.js
@@ -1,4 +1,8 @@
-var box1 = document.getElementById('button1');
+var button1 = document.getElementById('button1');
+
+
+
+
 button1.onmouseover = function(){
   button1.style.backgroundColor = "#d1cbcb";
 }
@@ -6,6 +10,22 @@ button1.onmouseout = function(){
   button1.style.backgroundColor = "yellow";
 }
 
+var button2 = document.getElementById('button2');
+
+button2.onmouseover = function(){
+  button3.style.backgroundColor = "#d1cbcb";
+}
+button2.onmouseout = function(){
+  button3.style.backgroundColor = "yellow";
+}
+
+var button3 = document.getElementById('button3');
+button3.onmouseover = function(){
+  button3.style.backgroundColor = "#d1cbcb";
+}
+button3.onmouseout = function(){
+  button3.style.backgroundColor = "yellow";
+}
 // var box2 = document.getElementById('box2');
 // box2.onmouseover = function(){
 //   box2.style.backgroundColor = "#d1cbcb";
