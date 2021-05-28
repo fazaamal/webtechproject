946b43b89170976a30a7f9891c5dfca319973eb3
ybdhani <ramadhani.nurfikri@gmail.com>
2021-05-29 01:13:13 +0800

    change

---
+++ scripts/AboutUsScript.js
@@ -0,0 +1,31 @@
+var box1 = document.getElementById('button1');
+button1.onmouseover = function(){
+  button1.style.backgroundColor = "#d1cbcb";
+}
+button1.onmouseout = function(){
+  button1.style.backgroundColor = "yellow";
+}
+
+// var box2 = document.getElementById('box2');
+// box2.onmouseover = function(){
+//   box2.style.backgroundColor = "#d1cbcb";
+// }
+// box2.onmouseout = function(){
+//   box2.style.backgroundColor = "#ffffff";
+// }
+//
+// var box3 = document.getElementById('box3');
+// box3.onmouseover = function(){
+//   box3.style.backgroundColor = "#d1cbcb";
+// }
+// box3.onmouseout = function(){
+//   box3.style.backgroundColor = "#ffffff";
+// }
+//
+// var box4 = document.getElementById('box4');
+// box4.onmouseover = function(){
+//   box4.style.backgroundColor = "#d1cbcb";
+// }
+// box4.onmouseout = function(){
+//   box4.style.backgroundColor = "#ffffff";
+// }
