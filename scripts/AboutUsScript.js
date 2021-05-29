c8c1382b8bdb4de0ee727e1f16e0ec32406d1037
ybdhani <ramadhani.nurfikri@gmail.com>
2021-05-30 01:52:00 +0800

    change

--- scripts/AboutUsScript.js
+++ scripts/AboutUsScript.js
@@ -24,6 +24,7 @@ var textbox1 = document.getElementById('textbox1');
     if (textbox1.style.display === 'none')
     {
       textbox1.style.display = 'block';
+      textbox2.style.display = 'none';
     }
     else
     {
@@ -34,6 +35,7 @@ var textbox1 = document.getElementById('textbox1');
 
 
 var button2 = document.getElementById('button2');
+var textbox2 = document.getElementById('textbox2');
 
   button2.onmouseover = function(){
     button2.style.backgroundColor = "#d1cbcb";
@@ -45,6 +47,7 @@ var button2 = document.getElementById('button2');
     if (textbox2.style.display === 'none')
     {
       textbox2.style.display = 'block';
+      textbox1.style.display = 'none';
     }
     else
     {
@@ -54,33 +57,29 @@ var button2 = document.getElementById('button2');
   }
 
 var button3 = document.getElementById('button3');
+// var textbox3 = document.getElementById('textbox3');
 
-  button3.onmouseover = function(){
-    button3.style.backgroundColor = "#d1cbcb";
-  }
-  button3.onmouseout = function(){
-    button3.style.backgroundColor = "yellow";
-  }
+    button3.onmouseover = function(){
+      button3.style.backgroundColor = "#d1cbcb";
+    }
+    button3.onmouseout = function(){
+      button3.style.backgroundColor = "yellow";
+    }
+    // button3.onclick = function(){
+    //   if (textbox3.style.display === 'none')
+    //    {
+    //     textbox3.style.display = 'block';
+    //     textbox1.style.display = 'none';
+    //    }
+    //   else
+    //    {
+    //     textbox3.style.display = 'none';
+    //    }
 
 
 
-
-
-// var button1 = document.getElementById('button1');
-//   button1.onmouseover = function(){
-//     button1.style.backgroundColor = "#d1cbcb";
-//   }
-//   button1.onmouseout = function(){
-//     button1.style.backgroundColor = "yellow";
-//   }
-// var button2 = document.getElementById('button2');
-//   button2.onmouseover = function(){
-//     button2.style.backgroundColor = "#d1cbcb";
-//   }
-//   button2.onmouseout = function(){
-//     button2.style.backgroundColor = "yellow";
-//   }
 // var button3 = document.getElementById('button3');
+//
 //   button3.onmouseover = function(){
 //     button3.style.backgroundColor = "#d1cbcb";
 //   }
