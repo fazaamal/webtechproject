fd44974ceeb16a8ed1b546643e122e5ccfa5b92f
Ramadhani <ramadhani.nurfikri@gmail.com>
2021-05-29 18:42:01 +0800

    change

--- scripts/AboutUsScript.js
+++ scripts/AboutUsScript.js
@@ -1,28 +1,58 @@
+// var textbox1 = document.getElementById('textbox1');
+//
+// function displayFunction() {
+//   if (textbox1.style.display === 'none')
+//   {
+//     textbox1.style.display = 'block';
+//   }
+//   else
+//   {
+//     textbox1.style.display = 'none';
+//   }
+// }
+
 var button1 = document.getElementById('button1');
+var textbox1 = document.getElementById('textbox1');
+
+  button1.onmouseover = function(){
+    button1.style.backgroundColor = "#d1cbcb";
+  }
+  button1.onmouseout = function(){
+    button1.style.backgroundColor = "yellow";
+  }
+  button1.onclick = function(){
+    // if (textbox1.style.display === 'none')
+    // {
+    //   textbox1.style.display = 'block';
+    // }
+    // else
+    // {
+      textbox1.style.display = 'none';
+    // }
+
+  }
+
+
 var button2 = document.getElementById('button2');
+
+  button2.onmouseover = function(){
+    button2.style.backgroundColor = "#d1cbcb";
+  }
+  button2.onmouseout = function(){
+    button2.style.backgroundColor = "yellow";
+  }
+
 var button3 = document.getElementById('button3');
 
+  button3.onmouseover = function(){
+    button3.style.backgroundColor = "#d1cbcb";
+  }
+  button3.onmouseout = function(){
+    button3.style.backgroundColor = "yellow";
+  }
 
-button1.onmouseover = function(){
-  button1.style.backgroundColor = "#d1cbcb";
-}
-button1.onmouseout = function(){
-  button1.style.backgroundColor = "yellow";
-}
 
-button2.onmouseover = function(){
-  button2.style.backgroundColor = "#d1cbcb";
-}
-button2.onmouseout = function(){
-  button2.style.backgroundColor = "yellow";
-}
 
-button3.onmouseover = function(){
-  button3.style.backgroundColor = "#d1cbcb";
-}
-button3.onmouseout = function(){
-  button3.style.backgroundColor = "yellow";
-}
 
 // var button1 = document.getElementById('button1');
 //   button1.onmouseover = function(){
