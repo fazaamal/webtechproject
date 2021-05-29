07d306a7d653db459290304fe2207a12fc678ec9
ybdhani <ramadhani.nurfikri@gmail.com>
2021-05-29 17:34:40 +0800

    change

--- scripts/AboutUsScript.js
+++ scripts/AboutUsScript.js
@@ -23,26 +23,25 @@ button3.onmouseover = function(){
 button3.onmouseout = function(){
   button3.style.backgroundColor = "yellow";
 }
-// var box2 = document.getElementById('box2');
-// box2.onmouseover = function(){
-//   box2.style.backgroundColor = "#d1cbcb";
-// }
-// box2.onmouseout = function(){
-//   box2.style.backgroundColor = "#ffffff";
-// }
-//
-// var box3 = document.getElementById('box3');
-// box3.onmouseover = function(){
-//   box3.style.backgroundColor = "#d1cbcb";
-// }
-// box3.onmouseout = function(){
-//   box3.style.backgroundColor = "#ffffff";
-// }
-//
-// var box4 = document.getElementById('box4');
-// box4.onmouseover = function(){
-//   box4.style.backgroundColor = "#d1cbcb";
-// }
-// box4.onmouseout = function(){
-//   box4.style.backgroundColor = "#ffffff";
-// }
+
+// var button1 = document.getElementById('button1');
+//   button1.onmouseover = function(){
+//     button1.style.backgroundColor = "#d1cbcb";
+//   }
+//   button1.onmouseout = function(){
+//     button1.style.backgroundColor = "yellow";
+//   }
+// var button2 = document.getElementById('button2');
+//   button2.onmouseover = function(){
+//     button2.style.backgroundColor = "#d1cbcb";
+//   }
+//   button2.onmouseout = function(){
+//     button2.style.backgroundColor = "yellow";
+//   }
+// var button3 = document.getElementById('button3');
+//   button3.onmouseover = function(){
+//     button3.style.backgroundColor = "#d1cbcb";
+//   }
+//   button3.onmouseout = function(){
+//     button3.style.backgroundColor = "yellow";
+//   }
