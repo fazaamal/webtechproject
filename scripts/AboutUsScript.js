b6be81705e0dc94f9439d206d42cfe5d595b1b84
fazaamal <fazasophian123@gmail.com>
2021-05-30 10:50:05 +0800

    Scripts

--- scripts/AboutUsScript.js
+++ scripts/AboutUsScript.js
@@ -14,6 +14,7 @@
 var button1 = document.getElementById('button1');
 var textbox1 = document.getElementById('textbox1');
 var arrow1 = document.getElementById('arrow1');
+var header1 = document.getElementById('header1');
 
 button1.onmouseover = function(){
   button1.style.backgroundColor = "#d1cbcb";
@@ -31,6 +32,10 @@ button1.onclick = function(){
   arrow1.src = 'images/arrowleft.png';
   arrow2.src = 'images/arrowright.png';
   arrow3.src = 'images/arrowright.png';
+
+  header1.style.fontWeight = 'bold'
+  header2.style.fontWeight = 'normal'
+  header3.style.fontWeight = 'normal'
   // textbox2.style.backgroundColor = 'yellow';
   // textbox2.style.font-weight = 'normal';
 }
@@ -39,6 +44,7 @@ button1.onclick = function(){
 var button2 = document.getElementById('button2');
 var textbox2 = document.getElementById('textbox2');
 var arrow2 = document.getElementById('arrow2');
+var header2 = document.getElementById('header2');
 
 button2.onmouseover = function(){
   button2.style.backgroundColor = "#d1cbcb";
@@ -56,6 +62,10 @@ button2.onclick = function(){
   arrow2.src = 'images/arrowleft.png';
   arrow1.src = 'images/arrowright.png';
   arrow3.src = 'images/arrowright.png';
+
+  header2.style.fontWeight = 'bold'
+  header1.style.fontWeight = 'normal'
+  header3.style.fontWeight = 'normal'
   //   textbox1.style.backgroundColor = 'yellow';
   //   textbox1.style.font-weight = 'normal';
   //
@@ -64,6 +74,7 @@ button2.onclick = function(){
 var button3 = document.getElementById('button3');
 var textbox3 = document.getElementById('textbox3');
 var arrow3 = document.getElementById('arrow3');
+var header3 = document.getElementById('header3');
 
 button3.onmouseover = function(){
   button3.style.backgroundColor = "#d1cbcb";
@@ -91,6 +102,10 @@ button3.onclick = function(){
   arrow3.src = 'images/arrowleft.png';
   arrow1.src = 'images/arrowright.png';
   arrow2.src = 'images/arrowright.png';
+
+  header3.style.fontWeight = 'bold'
+  header2.style.fontWeight = 'normal'
+  header1.style.fontWeight = 'normal'
   //   textbox1.style.backgroundColor = 'yellow';
   //   textbox1.style.font-weight = 'normal';
   //
